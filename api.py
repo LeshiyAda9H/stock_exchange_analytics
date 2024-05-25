cb0f65c76840e876e188eafc0fda28e7ba3ee032
Denis <usikdenisik@yandex.ru>
2024-05-25 18:52:16 +0300

    upd

--- api.py
+++ api.py
@@ -1,23 +1,21 @@
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Получение данных через API MOEX
-url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json'
+url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/MOEX.json?from=2023-01-01&till=2023-01-31&marketprice_board=1'
+response = requests.get(url)
+data = response.json()
+# Преобразование данных в pandas DataFrame
+securities_data = data['history']
+df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
+df.to_csv('one_company_data.csv', index=False)
+
+# Получение данных через API MOEX
+url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities.json'
 response = requests.get(url)
 data = response.json()
 
 # Преобразование данных в pandas DataFrame
-securities_data = data['securities']
+securities_data = data['history']
 df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
-df.to_csv('df.csv', index=False)
-
-# Получение данных через API MOEX
-url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/columns.json'
-response = requests.get(url)
-data = response.json()
-
-# Преобразование данных в pandas DataFrame
-securities_data = data['securities']
-df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
-df.to_csv('columns.csv', index=False)
+df.to_csv('all_companies.csv', index=False)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-
+import pandas as pd
+import matplotlib as mpl
 
 def main():
-
+    df = pd.read_csv('one_company_data.csv')
+    print(abs(df['LEGALCLOSEPRICE'] - df['OPEN']))
     return 0
 
 if __name__ == '__main__':
