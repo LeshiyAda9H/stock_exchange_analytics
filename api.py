f4f4e0c9fcdd624f9f531d16b36ea566b31aafba
Denis <usikdenisik@yandex.ru>
2024-05-25 20:31:33 +0300

    loading id db

--- api.py
+++ api.py
@@ -2,15 +2,6 @@ import requests
 import pandas as pd
 
 # Получение данных через API MOEX
-url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/MOEX.json?from=2023-01-01&till=2023-01-31&marketprice_board=1'
-response = requests.get(url)
-data = response.json()
-# Преобразование данных в pandas DataFrame
-securities_data = data['history']
-df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
-df.to_csv('one_company_data.csv', index=False)
-
-# Получение данных через API MOEX
 url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities.json'
 response = requests.get(url)
 data = response.json()
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import requests
 import pandas as pd
 import matplotlib as mpl
 
+start_date = '2023-01-01'
+end_date = '2023-01-31'
+
+def analyse(id):
+    url = ('https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/' + id + '.json?from='
+           + start_date + '&till=' + end_date + '&marketprice_board=1')
+    response = requests.get(url)
+    data = response.json()
+    securities_data = data['history']
+    df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
+
 def main():
-    df = pd.read_csv('one_company_data.csv')
-    print(abs(df['LEGALCLOSEPRICE'] - df['OPEN']))
+    df = pd.read_csv('all_companies.csv')
+    companies = df['SECID']
+    for id in companies:
+        analyse(id)
     return 0
 
 if __name__ == '__main__':
