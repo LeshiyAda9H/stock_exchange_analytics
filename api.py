b219e84f9fa942590b4f4a064f9fc5915cf933b7
Denis <usikdenisik@yandex.ru>
2024-05-25 14:43:52 +0300

    add df

---
+++ api.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Получение данных через API MOEX
+url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json'
+response = requests.get(url)
+data = response.json()
+
+# Преобразование данных в pandas DataFrame
+securities_data = data['securities']
+df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
+df.to_csv('df.csv', index=False)
