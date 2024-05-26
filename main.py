1ac5acdbbff92fcd333a79c404c43e5b107403ce
Denis <usikdenisik@yandex.ru>
2024-05-26 19:55:04 +0300

    pe upd

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from listlevels import *
 from pe import *
 import json
 import pandas as pd
+import warnings
 
 def main():
+    warnings.filterwarnings("ignore")
     #method1 = Volume()
     #method2 = ListLevels()
     method3 = PE()
+    method3.bottom()
     return 0
 
 if __name__ == '__main__':
--- pe.py
+++ pe.py
@@ -4,20 +4,26 @@ import numpy as np
 import matplotlib as mpl
 
 class PE:
+    def bottom(self):
+        df = pd.DataFrame(self.pes, columns = ['Name', 'ID', 'Capitalization', 'Income', 'P/E ratio'])
+        df.to_csv('databases/PE.csv')
+
     def __init__(self):
         self.pes = []
         c1 = set()
         c2 = set()
         marketdata = pd.read_csv('databases/marketdata.csv')
         incomes = pd.read_csv('databases/income.csv')
+        securities = pd.read_csv('databases/securities_data.csv')
         for x in marketdata['SECID']:
             cap = float(marketdata.loc[marketdata['SECID'] == x]['ISSUECAPITALIZATION'])
             if np.isnan(cap):
                 continue
             try:
                 income = float(incomes.loc[incomes['SECID'] == x]['INCOME'])
-                self.pes.append(cap / income / 1000000000)
+                pe = cap / income / 1000000000
+                if pe > 0:
+                    self.pes.append([list(securities.loc[securities['SECID'] == x]['SECNAME'])[0],
+                    x, cap, income, pe])
             except:
                 pass
-        for x in self.pes:
-            print(x)
