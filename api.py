b2e941036e31c1ba9eb59e8c68bf9a1b5da34581
Denis <usikdenisik@yandex.ru>
2024-05-26 14:19:12 +0300

    dbs

--- api.py
+++ api.py
@@ -1,19 +1,16 @@
 import requests
 import pandas as pd
 
-url = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/securities.json'
-response = requests.get(url)
-data = response.json()
-history_data = data['history']
-df = pd.DataFrame(history_data['data'], columns=history_data['columns'])
-df.to_csv('history_data.csv', index=False)
+data = requests.get('https://iss.moex.com/iss/history/engines/stock/markets/shares/securities.json').json()
+pd.DataFrame(data['history']['data'], columns=data['history']['columns']).to_csv('databases/history_data.csv', index=False)
 
-url = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json'
-response = requests.get(url)
-data = response.json()
-securities_data = data['securities']
-df = pd.DataFrame(securities_data['data'], columns=securities_data['columns'])
-df.to_csv('securities_data.csv', index=False)
-marketdata = data['marketdata']
-df = pd.DataFrame(marketdata['data'], columns=marketdata['columns'])
-df.to_csv('marketdata.csv', index=False)
+data = requests.get('https://iss.moex.com/iss/history/engines/stock/markets/shares/securities/columns.json').json()
+pd.DataFrame(data['history']['data'], columns=data['history']['columns']).to_csv('columns_descriptions/history_data.csv', index=False)
+
+data = requests.get('https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.json').json()
+pd.DataFrame(data['securities']['data'], columns=data['securities']['columns']).to_csv('databases/securities_data.csv', index=False)
+pd.DataFrame(data['marketdata']['data'], columns=data['marketdata']['columns']).to_csv('databases/marketdata.csv', index=False)
+
+data = requests.get('https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/columns.json').json()
+pd.DataFrame(data['securities']['data'], columns=data['securities']['columns']).to_csv('columns_descriptions/securities_data.csv', index=False)
+pd.DataFrame(data['marketdata']['data'], columns=data['marketdata']['columns']).to_csv('columns_descriptions/marketdata.csv', index=False)
