1398314cd20c4cd84766bb5eb16b02a0f108112b
Denis <usikdenisik@yandex.ru>
2024-05-26 16:05:52 +0300

    Update listlevels.py

--- listlevels.py
+++ listlevels.py
@@ -7,8 +7,9 @@ class ListLevels:
     def __init__(self):
         df = pd.read_csv('databases/securities_data.csv')
         self.level1 = df.loc[df['LISTLEVEL'] == 1]['SECID']
-        self.level2 = df.loc[df['LISTLEVEL'] == 1]['SECID']
-        self.level3 = df.loc[df['LISTLEVEL'] == 1]['SECID']
+        self.level2 = df.loc[df['LISTLEVEL'] == 2]['SECID']
+        self.level3 = df.loc[df['LISTLEVEL'] == 3]['SECID']
+        self.levels = df['LISTLEVEL']
 
     def graph(self):
         pass
